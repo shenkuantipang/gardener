80cbc76857de5d26a492a7cc978f1d06310798bb
Stefan Cimander <stefan.cimander@me.com>
2020-06-24 20:29:23 +0200

    Setup multiplatform Xcode app project template

---
+++ Shared/GardenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardenerApp.swift
+//  Shared
+//
+//  Created by Stefan Cimander on 24.06.20.
+//
+
+import SwiftUI
+
+@main
+struct GardenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
